437b967e6f77ff5ab3b2dfda82f3ee176fe8f5fc
Aayush Gupta <aayushgupta219@gmail.com>
2020-12-28 19:29:17 +0530

    certus: Drop charge_logger service & binary
    Dumps charging related status but requires horrible amount
    of SELinux permissions
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I3be0f0eca86332e2a99fe0ee70b941aad7e85ea3

--- certus-vendor.mk
+++ certus-vendor.mk
@@ -22,7 +22,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/certus/proprietary/vendor/bin/ccci_fsd:$(TARGET_COPY_OUT_VENDOR)/bin/ccci_fsd \
     vendor/xiaomi/certus/proprietary/vendor/bin/ccci_mdinit:$(TARGET_COPY_OUT_VENDOR)/bin/ccci_mdinit \
     vendor/xiaomi/certus/proprietary/vendor/bin/ccci_rpcd:$(TARGET_COPY_OUT_VENDOR)/bin/ccci_rpcd \
-    vendor/xiaomi/certus/proprietary/vendor/bin/charge_logger:$(TARGET_COPY_OUT_VENDOR)/bin/charge_logger \
     vendor/xiaomi/certus/proprietary/vendor/bin/charon:$(TARGET_COPY_OUT_VENDOR)/bin/charon \
     vendor/xiaomi/certus/proprietary/vendor/bin/dconfig:$(TARGET_COPY_OUT_VENDOR)/bin/dconfig \
     vendor/xiaomi/certus/proprietary/vendor/bin/dtc_vendor:$(TARGET_COPY_OUT_VENDOR)/bin/dtc_vendor \
